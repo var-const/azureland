93b4563e7ef0003b07c6a3d19cf71c127fa89999
kv <devnull@localhost>
2014-01-18 00:48:41 +0700

    Bugfix (negative indexes in spatial grid)

--- src/cri_spatial_grid.h
+++ src/cri_spatial_grid.h
@@ -46,7 +46,7 @@ void CRISpatialGrid<MaxRows, MaxCols>::Reinit( const ObjIterT Begin,
     const ObjIterT End, const float Time )
 {
     using ci::Vec2i;
-    using std::min; using std::pair;
+    using std::max; using std::min; using std::pair;
     
     assert(m_CellSize != Vec2i::zero());
 
@@ -64,11 +64,11 @@ void CRISpatialGrid<MaxRows, MaxCols>::Reinit( const ObjIterT Begin,
             GetMovementBounds(**ObjIter, Time);
         const Vec2i LeftUpper = MovementBounds.first / m_CellSize;
         const Vec2i RightLower = MovementBounds.second / m_CellSize;
-        for (int Row = LeftUpper.y; Row < min(MaxRows, RightLower.y + 1);
+        for (int Row = max(LeftUpper.y, 0); Row < min(MaxRows, RightLower.y + 1);
             ++Row)
         {
-            for (int Col = LeftUpper.x; Col < min(MaxCols, RightLower.x + 1);
-                ++Col)
+            for (int Col = max(LeftUpper.x, 0);
+                Col < min(MaxCols, RightLower.x + 1); ++Col)
             {
                 m_Cells[Row][Col].push_back(*ObjIter);
             }

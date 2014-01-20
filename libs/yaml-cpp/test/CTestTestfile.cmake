d947040ff521d6d856b9d84783b6dcdca9594e47
kv <devnull@localhost>
2014-01-20 22:11:58 +0700

    Added yaml-cpp

---
+++ libs/yaml-cpp/test/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/work/boomzap/crimsonland/libs/yaml-cpp/test
+# Build directory: C:/work/boomzap/crimsonland/libs/yaml-cpp/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(yaml-reader-test "run-tests")
---
+++ libs/yaml-cpp/util/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/work/boomzap/crimsonland/libs/yaml-cpp/util
+# Build directory: C:/work/boomzap/crimsonland/libs/yaml-cpp/util
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

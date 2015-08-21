3f3e93a6639bd39109b5925e9614cfd104dbd17c
vk <vk@localhost>
2015-08-21 23:16:53 +0600

    Add a template function to compare integers and floats in a uniform way

--- src/cri_math.h
+++ src/cri_math.h
@@ -4,3 +4,19 @@
 
 bool IsFpointEq(float LHS, float RHS, float Eps =
     std::numeric_limits<float>::epsilon() );
+
+template <typename T, typename std::enable_if_t<
+    std::is_integral<T>::value>*
+    = nullptr>
+bool is_equal(T lhs, T rhs)
+{
+    return lhs == rhs;
+}
+
+template <typename T, typename std::enable_if_t<
+    std::is_floating_point<T>::value>*
+    = nullptr>
+bool is_equal(T lhs, T rhs)
+{
+    return IsFpointEq(lhs, rhs);
+}

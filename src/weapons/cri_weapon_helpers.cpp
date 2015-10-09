67b97add3e7845303fc2ab4cc6e8ecc8ce4738e7
vk <vk@localhost>
2015-10-09 19:51:38 -0400

    [sdl] weapon_helpers compiles

--- src/weapons/cri_weapon_helpers.cpp
+++ src/weapons/cri_weapon_helpers.cpp
@@ -1,14 +1,14 @@
-#include "cri_stdafx.h"
+#include "../cri_stdafx.h"
 
 #include "cri_weapon_helpers.h"
 
-#include "math_vector.h"
+#include "../math_vector.h"
 
 
 
 Vec2f GetTargetVector( const Vec2f StartPos, const Vec2f TargetPos )
 {
     Vec2f Result = TargetPos - StartPos;
-    Result.safeNormalize();
+    Result.safe_normalize();
     return Result;
 }
